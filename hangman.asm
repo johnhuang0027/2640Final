34e238a99c57afa0fe48af4b6805d9f5f795521d
Joshua Gomez <128662224+bigboybosh@users.noreply.github.com>
2023-05-10 21:45:38 -0700

    Update2.0 hangman.asm
    I finally managed to implement a tracker for the number of attempts remaining in the code. Also, I managed to successfully print out all found chars within the string while printing the blank spaces. Yet, I'm still hardcoding the guessing word since I'm not 100% sure how to get around that. So that's one area that needs to be addressed.

--- hangman.asm
+++ hangman.asm
@@ -5,10 +5,10 @@
 #objectives:
 # - create a game of Hangman (allowed up to 7 attempts)
 # NOTES:
-# - need to implement a tracker for however many attempts the player has left. 
 # - if possible, make use of the bit map display in order to make the game more lively and fun
 # - as of right now, the words are hardcoded into the program
-
+#
+################	macro segment
 .macro printStr(%str)
 	li $v0, 4
 	la $a0, %str
@@ -18,7 +18,12 @@
 .macro readChar
 	li $v0, 12
 	syscall
-	move $s0, $v0
+.end_macro
+
+.macro printChar(%char)
+	li $v0, 11
+	move $a0, %char
+	syscall
 .end_macro
 
 .macro readInt
@@ -33,48 +38,117 @@
 	syscall
 .end_macro
 
+################	data segment
 .data
-word1: .asciiz "word"
+#	words
+word1: .asciiz "bomb"
 
+#	prompts
 welcome: .asciiz "\nWelcome to The Hangman\n\nCan you guess the word?"
 enterLetter: .asciiz "\n\nEnter a letter: "
-yes: .asciiz "\nLetter was found"
-no: .asciiz "\nLetter was not found"
+newline: .asciiz "\n"
+blanks: .asciiz " _ "
+yes: .asciiz " was found."
+no: .asciiz " was not found."
 tryAgain: .asciiz "\nTry again? (1 for yes, 2 for no) : "
+goodbye: .asciiz "\nThanks for playing, goodbye!"
+numAttempts: .asciiz "\nAttempts left: "
+totAttempts: .asciiz "\nYour total attempts remaining were: "
+youLost: .asciiz "\n\nYou lost. The hangman is finished.\nThe word was: "
+youWon: .asciiz "\n\nYou won! The hangman is spared.\nThe word was: "
 
+################	text segment
 .text
 	printStr(welcome)
+	li  $t7, 7	#tracks number of attempts
+	add $t6, $t6, $zero	#tracks num of chars found
 main:
 	printStr(enterLetter)
 	readChar
-	la $a1, word1	#load address of word1
-	li $s1, 0	#set counter
+	move $s0, $v0	#stores char in $s0
+	la   $a1, word1	#load address of word1
+	li   $s1, 0	#set counter
 	
+#	loop to check whether or not the string contains the char
 findLetter:
-	beq $s1, 4, notFound	#if counter reaches 4, go to notFound
-	lbu $a0, ($a1)		#load char at base address $s1
+	beq $s1, 4, notFound		#if counter reaches 4, go to notFound
+	lbu $a0, ($a1)			#load char at base address $s1
 	
-	beq $s0, $a0, found	#if they match, go to found
+	beq $s0, $a0, resetCounter	#if current address matches char, go to found
 	
-	add $s1, $s1, 1		#increment counter
-	add $a1, $a1, 1		#increment address
+	add $s1, $s1, 1			#increment counter
+	add $a1, $a1, 1			#increment address
 	
 	j findLetter
 	
+resetCounter:
+	move $s1, $zero
+	la   $a1, word1
+	printStr(newline)
+	
+#	loop to print blank spaces, as well as the chars found within the string
+printBlanks:
+	beq $s1, 4, found	#once counter reaches 4, go to found
+	lbu $a0, ($a1)		#load char at base address $s1
+	beq $s0, $a0, printIt	#if they match, go to printIt
+	
+	printStr(blanks)
+	add $s1, $s1, 1		#increment counter
+	add $a1, $a1, 1		#increment address
+	
+	j printBlanks
+	
+printIt:
+	printChar($s0)
+	add $t6, $t6, 1		#increment num of chars found
+	add $s1, $s1, 1		#increment counter
+	add $a1, $a1, 1		#increment address
+	j printBlanks
+	
 found:
+	beq $t6, 4, winner	#once all chars in the string are found, you win
+	printStr(newline)
+	printChar($s0)
 	printStr(yes)
-	j retry
+	
+	printStr(numAttempts)	#print out number of attempts left
+	printInt($t7)
+	j main
 	
 notFound:
+	printStr(newline)
+	printChar($s0)
 	printStr(no)
+	
+	printStr(numAttempts)
+	sub $t7, $t7, 1
+	printInt($t7)
+	beq $t7, $zero, loser	#once num of tries reachez 0, you lose
+	j main
+
+winner:
+	printStr(youWon)
+	printStr(word1)
+	printStr(totAttempts)
+	printInt($t7)
+	j retry
+	
+loser:
+	printStr(youLost)
+	printStr(word1)
+	printStr(totAttempts)
+	printInt($t7)
 	j retry
 	
 retry:
+	li   $t7, 7	#reset num of attempts
+	li   $t6, 0	#resent num of chars found
 	printStr(tryAgain)
 	readInt
-	beq $s0, 1, main
-	beq $s0, 2, exit
+	beq  $v0, 1, main
+	beq  $v0, 2, exit
 	
 exit:
+	printStr(goodbye)
 	li $v0, 10
 	syscall
